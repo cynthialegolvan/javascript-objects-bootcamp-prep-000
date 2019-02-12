a521ca6dbb2693ae437795f940b30e18b0ecb9ed
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-12 08:13:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {alesso: "let me go"};
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist [artistName] = songTitle
+  playlist [artistName] = songTitle;
+  return playlist
 }
